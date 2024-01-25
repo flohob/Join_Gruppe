0c71f7528a8b877783ab21764c9ab80aa23b29bc
Florian Hobiger <florianhobiger@MacBook-Pro-von-Florian.local>
2024-01-25 09:25:51 +0100

    Overlay + Main Contacts

---
+++ js/contacts.js
@@ -0,0 +1,7 @@
+function closeOverlay() {
+    document.getElementById('overlay_addcontact_main').classList.add('d-none');
+}
+
+function showOverlay() {
+    document.getElementById('overlay_addcontact_main').classList.remove('d-none');
+}
