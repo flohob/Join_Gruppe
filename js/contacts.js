610e8e6fe87669130fb0c862614c357736b72182
Florian Hobiger <florianhobiger@MacBook-Pro-von-Florian.local>
2024-02-01 16:20:31 +0100

    Contacts JS Logic

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,4 @@
+let currentEditIndex;
 let contacts = [ {
   id: generateUniqueId(),
   name: "Pascal Ganglberger",
@@ -11,7 +12,6 @@ let contacts = [ {
   phone: "+49 123 456789",
 },];
 
-let currentEditIndex;
 
 function initContacts() {
   loadContacts();
@@ -37,7 +37,6 @@ async function loadContacts() {
   } catch (error) {
       console.error('Loading error:', error);
   }
-  
   renderContacts();
 }
 
@@ -66,14 +65,9 @@ function renderContacts() {
   const contactContentDiv = document.getElementById('contact_content_2');
   contactContentDiv.innerHTML = '';
   sortJSON();
-
-  // Iteriere über jeden Kontakt und füge das HTML dem Div hinzu
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-
-    // Extrahiere die ersten Buchstaben des Vor- und Nachnamens
     const initials = contact.name.split(' ').map(word => word[0]).join('');
-
     const contactHtml = `
       <div class="contact" onclick="showContactInfo(${i})">
         <div>
@@ -93,8 +87,6 @@ function renderContacts() {
         </div>
       </div>
     `;
-
-    // Füge das Kontakt-HTML dem Div hinzu
     contactContentDiv.innerHTML += contactHtml;
   }
 }
@@ -119,7 +111,7 @@ function showContactInfo(index) {
         </svg>
         <div>
           <h2>${contact.name}</h2>
-          <div class="main_icons onclick="editContact(${index})"">
+          <div class="main_icons" onclick="editContact(${index})">
             <img src="assets/Login/edit.png"  alt="" /> <span>Edit</span>
             <img src="assets/Login/delete.png" alt="" /> <span>Delete</span>
           </div>
@@ -133,26 +125,17 @@ function showContactInfo(index) {
       <h2>Phone</h2>
       <span>${contact.phone}</span>
     </div>
-  `
-  ;
-
-  // Aktualisiere die "contact_info_main"
+  `;
   contactInfoDiv.innerHTML = contactInfoHtml;
 }
 
 function editContact(index) {
   const contact = contacts[index];
   console.log(contact);
-
-  // Fülle die Bearbeitungsfelder mit den aktuellen Werten des Kontakts
   document.getElementById('editcontact_fullname').value = contact.name;
   document.getElementById('editcontact_email').value = contact.email;
   document.getElementById('editcontact_phone').value = contact.phone;
-
-  // Speichere den Index des zu bearbeitenden Kontakts
   currentEditIndex = index;
-
-  // Öffne das Overlay für die Bearbeitung
   toggleEditOverlay();
 }
 
@@ -160,8 +143,6 @@ function saveEditedContact() {
   const editedName = document.getElementById('editcontact_fullname').value;
   const editedEmail = document.getElementById('editcontact_email').value;
   const editedPhone = document.getElementById('editcontact_phone').value;
-
-  // Aktualisiere den Kontakt im Array
   contacts[currentEditIndex].name = editedName;
   contacts[currentEditIndex].email = editedEmail;
   contacts[currentEditIndex].phone = editedPhone;
@@ -172,21 +153,15 @@ function saveEditedContact() {
 
 function deleteContact() {
   if (confirm('Are you sure you want to delete this contact?')) {
-    // Bestätigung des Benutzers für das Löschen
-    contacts.splice(currentEditIndex, 1); // Entferne den Kontakt aus dem Array
-    saveContacts(); // Speichere die aktualisierten Kontakte
-    renderContacts(); // Aktualisiere die Kontaktliste
+    contacts.splice(currentEditIndex, 1);
+    saveContacts();
+    renderContacts();
   }
 }
-
-
-
 
 function sortJSON () {
   contacts.sort((a, b) => a.name[0].localeCompare(b.name[0]));
 }
-
-
 
 function toggleOverlay() {
   var overlay = document.getElementById('overlay_addcontact_main');
